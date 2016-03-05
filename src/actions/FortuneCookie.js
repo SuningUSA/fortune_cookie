928ff84585e9b6f7eb494a7cff237fa5e84a0bb1
David Huynh <david.huynh@ussuning.com>
2016-03-04 18:27:58 -0800

    - added reset button - updated ui, refactored states - Updated UI, - Fixed JSLint issues - Removed favicon

--- src/actions/FortuneCookie.js
+++ src/actions/FortuneCookie.js
@@ -1,12 +1,12 @@
 var request = require('superagent');
-var url = "http://fortunecookieapi.com/v1/fortunes?limit=100&skip=0";
+var url = 'http://fortunecookieapi.com/v1/fortunes?limit=1000&skip=0';
 module.exports= {
 	getFortunes: function(){
-		return new Promise(function(resolve, reject){
+		return new Promise(function(resolve){
 			request.get(url)
 			.set('Accept', 'application/json')
 			.end(function(err, response){
-				if(err) return console.error(err);
+				if(err) return err;
 				else resolve(JSON.parse(response.text));
 			});
 		});
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,47 +7,65 @@ import Jumbotron from 'react-bootstrap/lib/Jumbotron';
 import FortuneCookie from '../actions/FortuneCookie';
 var cookieTitle = require('../images/chibi_cookie.png');
 var brokenCookie = require('../images/chibi_cookie_broken.png');
+var initialState= {
+			cookie: cookieTitle,
+			broken: 'show',
+			unbroken: 'hidden',
+			fortune: ''
+};
+var activeState = {
+  		cookie: brokenCookie,
+  		broken: 'hidden',
+  		unbroken: 'show'
+};
+
 class AppComponent extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {
-			cookie: props.initCookie,
-			broken: 'show',
-			fortune: ''
-		};
+		this.state = initialState;
 	}
   breakCookie(){
-  	  		this.setState({
-  		cookie: brokenCookie,
-  		broken: 'hidden'
-  		// fortune: Math.floor(Math.random() * Object.keys(data).length);
-  	});
+  	  		this.setState(activeState);
   	FortuneCookie.getFortunes().then(data =>{
   		let randomMessage = data[ Math.floor(Math.random() * Object.keys(data).length)];
   		this.setState({fortune:  randomMessage.message});
   	});
 
   }
+  reset(){
+  	this.setState(initialState);
+  }
   render() {
     return (
-      <Jumbotron className='text-center'>
-      	<div>
-        	<h2>Fortune Cookie</h2>
-        	<p>
-        		<span className={!this.state.broken}>{this.state.fortune}</span>
-        		<span className={this.state.broken}>Break the cookie and find out</span>
-        	</p>
-        	<img className="max-image-size cursor-pointer" src={this.state.cookie} onClick={this.breakCookie.bind(this)}/>
-        </div>
-        <div>
-        </div>
-      </Jumbotron>
+    <div>
+      	<Jumbotron className='text-center'>
+      		<div>
+        		<h2>Fortune Cookie</h2>
+        		<small>Presented by: Cloud Lab</small>
+        		<h3 className='{this.state.unbroken} cookie-fortune'>{this.state.fortune}</h3>
+        		<p>
+        			<span className={this.state.broken}>Break the cookie and find out</span>
+        		</p>
+        			<img className="max-image-size cursor-pointer" src={this.state.cookie} onClick={this.breakCookie.bind(this)}/>
+        		<p>
+        			<a href='#' className={this.state.unbroken} onClick={this.reset.bind(this)}>Try Another</a>
+        		</p>
+        	</div>
+      	</Jumbotron>
+      	<div className='footer'>
+      		<span>
+      			Suning USA ©2016
+      		</span>
+      		<span className='pull-right'>
+      			845 Page Mill RD, CA 94304
+      		</span>
+      	</div>
+      </div>
     );
   }
 }
 
 AppComponent.defaultProps = {
-	initCookie: cookieTitle
 };
 
 export default AppComponent;
